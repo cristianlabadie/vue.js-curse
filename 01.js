8369bdb2fd6e09e78dca00fde12ed46be473defb
cristian <cristian.labadie@gmail.com>
2019-07-25 23:26:48 -0400

    implement vuejs basic interacctions

---
+++ 01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola mundo desde Vuejs',
+        frutas: ['Manzana','Pera','Frutilla'],
+        personas: [
+            {nombre:'Cristian',edad:23},
+            {nombre:'Javiera',edad:22}
+        ]
+    }
+})
